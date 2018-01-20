2a769d3dd29036287be8ff0e89b4439b5503a37b
tomshapira <tom.shapira@whitesourcesoftware.com>
2018-01-20 12:33:46 +0200

    Remove file counter code

--- src/main/java/org/whitesource/demo/Main.java
+++ src/main/java/org/whitesource/demo/Main.java
@@ -7,27 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         String input = args[0];
-
-        System.out.println(mySwapCase(input));
-
-//        System.out.println(StringUtils.swapCase(input));
-
-//        new ExtensionFileCounter().countFilesInDirectoryByExtension(args[0], args[1]);
-    }
-
-    private static String mySwapCase(String string) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < string.length(); i++) {
-            String letter = String.valueOf(string.charAt(i));
-            if (letter.matches("[a-z]")) {
-                result.append(letter.toUpperCase());
-            } else if (letter.matches("[A-Z]")) {
-                result.append(letter.toLowerCase());
-            } else {
-                result.append(letter);
-            }
-        }
-        return result.toString();
+        System.out.println("You've entered the following text: " + input);
     }
 
 }
