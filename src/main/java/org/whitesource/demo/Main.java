1348253a360225e424ff66bb0adbc260e731ac7c
tomshapira <tom.shapira@whitesourcesoftware.com>
2018-01-20 13:11:49 +0200

    Add input validation

--- src/main/java/org/whitesource/demo/Main.java
+++ src/main/java/org/whitesource/demo/Main.java
@@ -6,6 +6,10 @@ package org.whitesource.demo;
 public class Main {
 
     public static void main(String[] args) {
+        if (args.length == 0) {
+            return;
+        }
+
         String input = args[0];
         System.out.println("You've entered the following text: " + input);
     }
