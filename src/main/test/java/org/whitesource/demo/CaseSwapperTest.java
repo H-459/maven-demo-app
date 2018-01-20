84ca9de7c6d12fcba5d5dbf7990cbadbfba2ba20
tomshapira <tom.shapira@whitesourcesoftware.com>
2018-01-20 13:35:40 +0200

    Add junit and a test class

---
+++ src/main/test/java/org/whitesource/demo/CaseSwapperTest.java
@@ -0,0 +1,15 @@
+package org.whitesource.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author tom.shapira
+ */
+public class CaseSwapperTest {
+
+    @Test
+    public void testExample() {
+        Assert.assertEquals("true", Boolean.toString(true));
+    }
+}
