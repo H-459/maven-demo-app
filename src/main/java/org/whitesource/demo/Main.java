83fb978ba3310d62ce6177cea7d45ca40c373172
tomshapira <tom.shapira@whitesourcesoftware.com>
2018-01-23 14:13:12 +0200

    Implement swapCase and fix a bug

--- src/main/java/org/whitesource/demo/Main.java
+++ src/main/java/org/whitesource/demo/Main.java
@@ -11,7 +11,24 @@ public class Main {
         }
 
         String input = args[0];
-        System.out.println("You've entered the following text: " + input);
+        System.out.println(swapCase(input));
+    }
+
+    private static String swapCase(String text) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            String letter = String.valueOf(text.charAt(i));
+            if (letter.matches("[a-z]")) {
+                // swap each lowercase letter to uppercase
+                result.append(letter.toUpperCase());
+            } else if (letter.matches("[A-Z]")) {
+                // swap each uppercase letter to lowercase
+                result.append(letter.toLowerCase());
+            } else {
+                result.append(letter);
+            }
+        }
+        return result.toString();
     }
 
 }
